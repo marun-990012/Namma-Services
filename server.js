862eb1624ae7c59e360136c478f64f0eeeb74930
marun-990012 <marun990012@gmail.com>
2025-04-28 12:26:53 +0530

    register controller used in server file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import configureDb from './config/db.js';
+import userController from './app/controllers/user-controller.js'
 const app = express();
 const port=3040;
 configureDb();
 
-// app.post('/register')
+app.post('/register',userController.register);
 app.listen(port,()=>{
     console.log('Server is running on port',port);
 });
