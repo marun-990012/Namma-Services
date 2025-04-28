c1c83f3857f0b075fefa552bdf1589e2bba1037f
marun-990012 <marun990012@gmail.com>
2025-04-28 13:35:55 +0530

    login controller added in server file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import configureDb from './config/db.js';
 import userController from './app/controllers/user-controller.js'
+
 const app = express();
 const port=3040;
 configureDb();
 
+// Parse JSON bodies
+app.use(express.json());
+
 app.post('/register',userController.register);
+app.post('/login',userController.login);
 app.listen(port,()=>{
     console.log('Server is running on port',port);
 });
