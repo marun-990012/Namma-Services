68e9d288c637c3de2878880727e2be87a322b94a
marun-990012 <marun990012@gmail.com>
2025-04-29 18:38:43 +0530

    created  service category controller

---
+++ app/controllers/service-category-controller.js
@@ -0,0 +1,43 @@
+import Service from "../models/service-category-model.js";
+const serviceCategoryController={};
+
+// controller for create service category 
+serviceCategoryController.create = async (req,res)=>{
+    const {name,description} = req.body;
+    try{
+        const category = await Service.create({name,description});
+        return res.status(201).json(category);
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({error:"Something went wrong"});
+    }
+};
+
+//controller for list all the category
+serviceCategoryController.list = async(req,res)=>{
+    // res.json('hello');
+    try{
+        const categories = await Service.find();
+        return res.json(categories);
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({error:"Something went wrong"});
+    }
+};
+
+// service category update controller
+serviceCategoryController.update = async(req,res)=>{
+    const id = req.params.id;
+    const {name,description} = req.body;
+    try{
+        const category = await Service.findByIdAndUpdate(id,{name,description},{new:true});
+        if(!category){
+            return res.status(404).json({error:"Service Category is not found"});
+        }
+        return res.json(category);
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({error:"Something went wrong"});
+    }
+}
+export default serviceCategoryController;
