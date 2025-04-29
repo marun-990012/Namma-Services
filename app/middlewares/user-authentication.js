e8387aa9fba47efbdd5744f75e0b0a0c98076634
marun-990012 <marun990012@gmail.com>
2025-04-29 19:13:17 +0530

    user authentication middleware created

---
+++ app/middlewares/user-authentication.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+function authenticateUser(req,res,next){
+    const token = req.headers['authorization'];
+    try{
+    if(!token){
+        return res.status(401).json({error:"Token is required"});
+    }
+    const tokenData = jwt.verify(token,'Secret@123');
+    req.userId = tokenData.userId;
+    req.role = tokenData.role;
+    next();
+    }catch(error){
+        return res.status(401).json({error:error.message});
+    }
+}
+export default authenticateUser;
