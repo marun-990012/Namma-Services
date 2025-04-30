cba32bea5cc2f8ede12d4a8ce307ed09399fc94f
marun-990012 <marun990012@gmail.com>
2025-04-30 11:02:05 +0530

    created myJobPost controller

--- app/controllers/job-post-controller.js
+++ app/controllers/job-post-controller.js
@@ -4,7 +4,7 @@ const jobPostController={};
 jobPostController.create = async(req,res)=>{
     const {title,description,workLocation,salary,images} = req.body;
     try{
-        const jobPost = await Job.create({title,description,workLocation,salary,images});
+        const jobPost = await Job.create({title,description,workLocation,salary,images,postedBy:req.userId});
         return res.status(201).json(jobPost);
     }catch(error){
         console.log(error);
@@ -16,6 +16,19 @@ jobPostController.list = async(req,res)=>{
     try{
         const jobPosts = await Job.find();
         return res.json(jobPosts);
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({error:"Something went wrong"});
+    }
+};
+
+jobPostController.myJobPosts = async (req,res) =>{
+    try{
+        const posts = await Job.find({postedBy:req.userId});
+        if(posts.length<1){
+            return res.status(404).json({message:"You haven't created any posts yet."});
+        }
+        return res.json(posts);
     }catch(error){
         console.log(error);
         return res.status(500).json({error:"Something went wrong"});
