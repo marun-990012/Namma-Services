d5e6b39a743a3d9f50e8a1c148e3f465c2d19381
marun-990012 <marun990012@gmail.com>
2025-04-30 11:11:05 +0530

    middleware created for admin

--- app/middlewares/admin-create-middleware.js
+++ app/middlewares/admin-create-middleware.js
@@ -1,22 +1,9 @@
 
-// import User from '../models/user-model.js';
-// export default function createAdmin(){
-//     return async(_, res, next)=>{
-//         const totalUser= await User.countDocuments();
-//         if(totalUser==0){
-//             next();
-//         }else{
-//             return res.status(400).json({error:"Admin Already exits"});
-//         }
-//     }
-// } 
-
 import User from '../models/user-model.js';
 
 export default function createAdmin() {
     return async (req, res, next) => {
         const totalUser = await User.countDocuments();
-        // const user=await User.findOne({userType:req.body.userType});
         if (!(req.body.userType=='admin' && totalUser>0)) {
             next();
         } else {
