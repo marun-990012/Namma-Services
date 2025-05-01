e0c0aba51ffc402546f103b7d7658a2ec59e465e
marun-990012 <marun990012@gmail.com>
2025-05-01 10:29:15 +0530

    created cloudinaryConfig.js file

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from 'cloudinary';
+cloudinary.config({ 
+    cloud_name: 'dxludspye', 
+    api_key: '594237119177773', 
+    api_secret: 'TiogsJ8kmhz5Fm48y609f3m8Rrc' // Click 'View API Keys' above to copy your API secret
+});
+
+export default cloudinary;
