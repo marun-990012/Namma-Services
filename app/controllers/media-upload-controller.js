c57c1961ea14e7701b7c842cae4517bb680a5cf4
marun-990012 <marun990012@gmail.com>
2025-05-01 10:33:57 +0530

    created new media file uploading controller

---
+++ app/controllers/media-upload-controller.js
@@ -0,0 +1,41 @@
+
+import cloudinary from "../../config/cloudinaryConfig.js";
+
+const mediaController = {};
+/**
+ * This function is used to Upload the image into cloudinary
+ * @param { file } - This  function takes images as a aggument upload it into cloudinary
+ * @returns - it returns the address of the image stored in cloudinary
+*/
+
+mediaController.uploadImage = async(req,res)=>{
+    try {
+          const file = req.file.path;
+      
+          const cloudinaryResponse = await cloudinary.uploader.upload(file, {
+            folder: 'classified_App_images',
+          });
+      
+          return res.json(cloudinaryResponse.secure_url);
+        } catch (err) {
+          console.error("Error uploading image to Cloudinary:", err);
+          return res.status(500).json({ error: "Failed to upload image" });
+        }
+}
+
+export default mediaController;
+
+// app.post('/upload', upload.single('file'), async(req,res)=>{
+//     try {
+//           const file = req.file.path;
+      
+//           const cloudinaryResponse = await cloudinary.uploader.upload(file, {
+//             folder: 'classified_App_images',
+//           });
+      
+//           return res.json(cloudinaryResponse);
+//         } catch (err) {
+//           console.error("Error uploading image to Cloudinary:", err);
+//           return res.status(500).json({ error: "Failed to upload image" });
+//         }
+// });
