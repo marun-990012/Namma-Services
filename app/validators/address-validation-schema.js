7ef1469212f811627c0ffcbb1c8b861c80aeafa1
marun-990012 <marun990012@gmail.com>
2025-05-02 18:08:53 +0530

    address validation schema created

---
+++ app/validators/address-validation-schema.js
@@ -0,0 +1,58 @@
+export const addressValidationSchema = {
+
+    //validation for street field
+    street:{
+        in:['body'],
+        exists:{
+            errorMessage:'street field is required'
+        },
+        notEmpty:{
+            errorMessage:'street field should not be empty'
+        },
+
+    },
+
+    //validation for city field
+    city:{
+        in:['body'],
+        exists:{
+            errorMessage:'city field is required'
+        },
+        notEmpty:{
+            errorMessage:'city field should not be empty'
+        }   
+    },
+
+    //validation for state field
+    state:{
+        in:['body'],
+        exists:{
+            errorMessage:'state field is required'
+        },
+        notEmpty:{
+            errorMessage:'state field should not be empty'
+        }  
+    },
+
+    //validation for postalCode field
+    postalCode:{
+        in:['body'],
+        exists:{
+            errorMessage:'postalCode field is required'
+        },
+        notEmpty:{
+            errorMessage:'postalCode field should not be empty'
+        }  
+    },
+
+    //validation for country field
+    country:{
+        in:['body'],
+        exists:{
+            errorMessage:'country field is required'
+        },
+        notEmpty:{
+            errorMessage:'country field should not be empty'
+        }  
+    }
+}
