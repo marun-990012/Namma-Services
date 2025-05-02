8b2bd2ead000e0a42d20e365598d9c9612884d13
marun-990012 <marun990012@gmail.com>
2025-05-02 18:16:40 +0530

    wallet route created

---
+++ config/routes/wallet-route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import authentication from '../../app/middlewares/user-authentication.js';
+import walletController from '../../app/controllers/wallet-controller.js';
+
+const walletRoute = express.Router();
+
+//api for wallet 3
+walletRoute.post('/create',authentication,walletController.createWallet);
+walletRoute.post('/add/coin',authentication,walletController.addCoin);
+walletRoute.post('/deduct/coin',authentication,walletController.deductCoin);
+
+export default walletRoute;
