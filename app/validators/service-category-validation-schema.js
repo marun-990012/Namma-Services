5ba3849476e6ac072a8e8bd53437683ed4a47567
marun-990012 <marun990012@gmail.com>
2025-05-02 18:07:12 +0530

    service category validation schema created

---
+++ app/validators/service-category-validation-schema.js
@@ -0,0 +1,42 @@
+import Service from "../models/service-category-model.js"
+
+//Service category validation schema
+export const serviceCategoryValidationSchema = {
+
+    //validation for name field
+    name:{
+        in:['body'],
+        exists:{
+            errorMessage:'name field is required'
+        },
+        notEmpty:{
+            errorMessage:'name field should not be empty'
+        },
+        trim:true,
+        custom:{
+            options:async function(value){
+                try{
+                    const service = await Service.findOne({name:value});
+                    if(service){
+                        throw new Error('service category already exists');
+                    }
+                }catch(error){
+                    throw new Error(error.message);
+                }
+                return true;
+            }
+        }
+    },
+
+    //validation for description field
+    description:{
+        in:['body'],
+        exists:{
+            errorMessage:'description field is required'
+        },
+        notEmpty:{
+            errorMessage:'description field should not be empty'
+        }
+    }
+
+}
