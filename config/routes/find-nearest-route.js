153f19cd7fc008bc5950b03960e7314f33a52d5c
marun-990012 <marun990012@gmail.com>
2025-05-05 10:46:51 +0530

    route created to find nearest work

--- config/routes/find-nearest-route.js
+++ config/routes/find-nearest-route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 
 import findNearestJobController from '../../app/controllers/find-nearest-job-controller.js';
+import authentication from '../../app/middlewares/user-authentication.js';
 
 const findJobRoute = express.Router();
 
-findJobRoute.get('/find',findNearestJobController.nearestJob);
+findJobRoute.get('/find/',authentication,findNearestJobController.nearestJob);
 export default findJobRoute;
