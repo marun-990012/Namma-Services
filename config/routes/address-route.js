841000094552cbed7ff536fbae8a7018f1fb16f0
marun-990012 <marun990012@gmail.com>
2025-05-05 10:47:39 +0530

    route created to add address

--- config/routes/address-route.js
+++ config/routes/address-route.js
@@ -4,8 +4,9 @@ import addressController from '../../app/controllers/address-controller.js';
 
 const addressRoute = express.Router();
 
-//api for address 2
+//api for address 3
 addressRoute.post('/create',authentication,addressController.createAddress);
 addressRoute.put('/update/:id',authentication,addressController.updateAddress);
+addressRoute.get('/find',authentication,addressController.find);
 
 export default addressRoute;
