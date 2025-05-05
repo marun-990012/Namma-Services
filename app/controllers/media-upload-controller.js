fcb2e1f0aac1993a5ea0341e289636e9012776a8
marun-990012 <marun990012@gmail.com>
2025-05-05 10:53:06 +0530

    meadia upload controller modified

--- app/controllers/media-upload-controller.js
+++ app/controllers/media-upload-controller.js
@@ -24,18 +24,3 @@ mediaController.uploadImage = async(req,res)=>{
 }
 
 export default mediaController;
-
-// app.post('/upload', upload.single('file'), async(req,res)=>{
-//     try {
-//           const file = req.file.path;
-      
-//           const cloudinaryResponse = await cloudinary.uploader.upload(file, {
-//             folder: 'classified_App_images',
-//           });
-      
-//           return res.json(cloudinaryResponse);
-//         } catch (err) {
-//           console.error("Error uploading image to Cloudinary:", err);
-//           return res.status(500).json({ error: "Failed to upload image" });
-//         }
-// });
