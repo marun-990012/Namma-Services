b5df5d50929cf9461ea0f83f311bae0fe44a468a
marun-990012 <marun990012@gmail.com>
2025-05-05 10:55:23 +0530

    payment controller created

---
+++ app/controllers/payment-controller.js
@@ -0,0 +1,9 @@
+// const Razorpay = require('razorpay');
+import Razorpay from 'razorpay';
+
+const razorpayInstance = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
+
+module.exports = razorpayInstance;
