01fe53f37e9fb525642db679ec572f75a7a600ed
marun-990012 <marun990012@gmail.com>
2025-05-05 23:06:29 +0530

    razorpayInstance config file created

---
+++ config/razorpayConfig.js
@@ -0,0 +1,6 @@
+
+import Razorpay from "razorpay";
+export const razorpayInstance = new Razorpay({
+    key_id: "rzp_test_cVi012fWsvhHkY",          // ✅ your Razorpay KEY ID (starts with rzp_test_)
+    key_secret: "nvv3jxWSqIrXxm2SbB2AlOIZ"      // ✅ your Razorpay SECRET
+  });
