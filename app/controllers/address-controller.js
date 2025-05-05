ef50e0c38887d57c70f51128c7d91d813c45c629
marun-990012 <marun990012@gmail.com>
2025-05-05 10:51:05 +0530

    address controller modified

--- app/controllers/address-controller.js
+++ app/controllers/address-controller.js
@@ -1,12 +1,23 @@
+import axios from "axios";
 import Address from "../models/address-model.js";
 const addressController = {};
 
 //add address controller
 addressController.createAddress = async(req,res)=>{
     const {street,city,state,postalCode,country} = req.body;
+    const apiKey = '414f3b4ff1ad47088654fae1b1c6ca01';
+    const encodedAddress = encodeURIComponent(`${street} ${city} ${postalCode} ${state} ${country}`);
+    const url = `https://api.geoapify.com/v1/geocode/search?text=${encodedAddress}&apiKey=${apiKey}`;
     try{
-        const address = await Address.create({street,city,state,postalCode,country,userId:req.userId});
-        return res.state(201).json(address);
+        const response = await axios.get(url);
+        const location = response.data.features[0].properties;
+
+        if(location){
+            const address = await Address.create({address:location.formatted,street,city,state,postalCode,country,location: {type: 'Point',coordinates: [location.lon, location.lat]},userId:req.userId});
+            return res.status(201).json(address);
+        }else{
+            return res.status(404).json({error:'enter valid address'});
+        }
     }catch(error){
         console.log(error);
         return res.status(500).json({error:'Something went wrong'});
@@ -17,8 +28,11 @@ addressController.createAddress = async(req,res)=>{
 addressController.updateAddress = async(req,res)=>{
     const id = req.params.id;
     const {street,city,state,postalCode,country} = req.body;
+    const apiKey = '414f3b4ff1ad47088654fae1b1c6ca01';
+    const encodedAddress = encodeURIComponent(`${street} ${city} ${postalCode} ${state} ${country}`);
+    const url = `https://api.geoapify.com/v1/geocode/search?text=${encodedAddress}&apiKey=${apiKey}`;
     try{
-        const address = await Address.findByIdAndUpdate(id,{street,city,state,postalCode,country},{new:true});
+        const address = await Address.findByIdAndUpdate(id,{address:location.formatted,street,city,state,postalCode,country,location: {type: 'Point',coordinates: [location.lon, location.lat]}},{new:true});
         if(!address){
             return res.status(404).json({error:'Record not found'});
         }
@@ -28,4 +42,14 @@ addressController.updateAddress = async(req,res)=>{
         return res.status(500).json({error:'Something went wrong'});
     }
 };
+
+addressController.find = async(req,res)=>{
+    try {
+        const address = await Address.findOne({userId:req.userId});
+        return res.json(address);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error:'Something went wrong'});
+    }
+}
 export default addressController;
