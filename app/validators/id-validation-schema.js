2f7b2289cb33e3ebf5d93c57b444fad4b901b0f2
marun-990012 <marun990012@gmail.com>
2025-05-06 22:35:11 +0530

    job post validation schema added

--- app/validators/id-validation-schema.js
+++ app/validators/id-validation-schema.js
@@ -12,3 +12,30 @@ export const idValidationSchema = {
         }
     }
 }
+
+export const reviewIdValidation = {
+    id: {
+        in: ['params'],
+        exists:{
+            errorMessage:'id field is required'
+        },
+        notEmpty:{
+            errorMessage:'id field should not be empty'
+        },
+        isMongoId: {
+          errorMessage: 'Invalid review document ID',
+        },
+      },
+      reviewid: {
+        in: ['params'],
+        exists:{
+            errorMessage:'reviewid field is required'
+        },
+        notEmpty:{
+            errorMessage:'reviewid field should not be empty'
+        },
+        isMongoId: {
+          errorMessage: 'Invalid review subdocument ID',
+        },
+      },
+}
--- app/validators/job-post-validation-schema.js
+++ app/validators/job-post-validation-schema.js
@@ -1,5 +1,5 @@
 //job post validation schema
-export const jobPostValidationSchema = {
+export const jobPostValidation = {
 
     //validation for title field
     title:{
@@ -10,6 +10,10 @@ export const jobPostValidationSchema = {
         notEmpty:{
             errorMessage:'title field should not be empty'
         }, 
+        isLength:{
+            options:{min:5,max:200},
+            errorMessage:'title should be between 5 to 200 character'
+        }
     },
 
     //validation for description field
@@ -21,6 +25,10 @@ export const jobPostValidationSchema = {
         notEmpty:{
             errorMessage:'description field should not be empty'
         }, 
+        isLength:{
+            options:{min:30,max:360},
+            errorMessage:'description should be between 30 to 360 character'
+        },
     },
 
     //validation for salary field
@@ -33,4 +41,158 @@ export const jobPostValidationSchema = {
             errorMessage:'salary field should not be empty'
         }, 
     },
+
+    address:{
+        in:['body'],
+        exists:{
+            errorMessage:'address field is required'
+        },
+        notEmpty:{
+            errorMessage:'address field should not be empty'
+        },
+        isLength:{
+            options:{min:20,max:360},
+            errorMessage:'address should be between 20 to 360 character'
+        },
+       
+    },
+
+    postalCode:{
+        in:['body'],
+        exists:{
+            errorMessage:'postalCode field is required'
+        },
+        notEmpty:{
+            errorMessage:'postalCode field should not be empty'
+        },
+    }
+};
+
+
+export const jobRequestValidation = {
+    serviceProvider: {
+      in: ['body'],
+      exists: {
+        errorMessage: 'serviceProvider field is required',
+      },
+      notEmpty: {
+        errorMessage: 'serviceProvider field should not be empty',
+      },
+    },
+    messages: {
+      in: ['body'],
+      exists: {
+        errorMessage: 'message field is required',
+      },
+      notEmpty: {
+        errorMessage: 'message field should not be empty',
+      },
+    },
+  };
+
+
+  export const sendMessageValidation = {
+    requestId: {
+        in: ['body'],
+        exists: {
+            errorMessage: 'requestId field is required',
+        },
+        notEmpty: {
+            errorMessage: 'requestId field should not be empty',
+        },
+        isMongoId: {
+            errorMessage: 'Invalid requestId format',
+        }
+    },
+    message: {
+        in: ['body'],
+        exists: {
+            errorMessage: 'message field is required',
+        },
+        notEmpty: {
+            errorMessage: 'message field should not be empty',
+        },
+        isObject: {
+            errorMessage: 'message should be an object',
+        },
+        custom: {
+            options: (value) => {
+                // Check if textMessage, name, and profileImage are present in the message object
+                if (!value.textMessage || !value.name || !value.profileImage) {
+                    throw new Error('message object is missing required fields (textMessage, name, profileImage)');
+                }
+                return true;
+            },
+        }
+    }
+};
+
+
+export const sendMessageReplyValidation = {
+    requestId:{
+        in: ['body'],
+        exists: {
+            errorMessage: 'requestId field is required',
+        },
+        notEmpty: {
+            errorMessage: 'requestId field should not be empty',
+        },
+        isMongoId: {
+            errorMessage: 'Invalid requestId format',
+        }
+    },
+    messageId:{
+        in: ['body'],
+        exists: {
+            errorMessage: 'messageId field is required',
+        },
+        notEmpty: {
+            errorMessage: 'messageId field should not be empty',
+        },
+        isMongoId: {
+            errorMessage: 'Invalid messageId format',
+        }
+    },
+    reply:{
+        in: ['body'],
+        exists: {
+          errorMessage: 'reply field is required',
+        },
+        notEmpty: {
+          errorMessage: 'reply field should not be empty',
+        },
+    }
+};
+
+
+export const considerationValidation = {
+    requestId:{
+        in: ['body'],
+        exists: {
+            errorMessage: 'requestId field is required',
+        },
+        notEmpty: {
+            errorMessage: 'requestId field should not be empty',
+        },
+        isMongoId: {
+            errorMessage: 'Invalid requestId format',
+        }
+    }
+}
+
+
+
+export const selectServiceProviderValidation = {
+    requestId:{
+        in: ['body'],
+        exists: {
+            errorMessage: 'requestId field is required',
+        },
+        notEmpty: {
+            errorMessage: 'requestId field should not be empty',
+        },
+        isMongoId: {
+            errorMessage: 'Invalid requestId format',
+        }
+    }
 }
