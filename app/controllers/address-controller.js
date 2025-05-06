3ddd3135dba3e729c800c609a71df008eb64f2a6
marun-990012 <marun990012@gmail.com>
2025-05-06 22:21:58 +0530

    in address controller find address controller added

--- app/controllers/address-controller.js
+++ app/controllers/address-controller.js
@@ -9,15 +9,21 @@ addressController.createAddress = async(req,res)=>{
     const encodedAddress = encodeURIComponent(`${street} ${city} ${postalCode} ${state} ${country}`);
     const url = `https://api.geoapify.com/v1/geocode/search?text=${encodedAddress}&apiKey=${apiKey}`;
     try{
-        const response = await axios.get(url);
-        const location = response.data.features[0].properties;
 
-        if(location){
-            const address = await Address.create({address:location.formatted,street,city,state,postalCode,country,location: {type: 'Point',coordinates: [location.lon, location.lat]},userId:req.userId});
-            return res.status(201).json(address);
-        }else{
-            return res.status(404).json({error:'enter valid address'});
+        const address = await Address.findOne({userId:req.userId});
+
+        if(!address){
+            const response = await axios.get(url);
+            const location = response.data.features[0].properties;
+            if(location){
+                const address = await Address.create({address:location.formatted,street,city,state,postalCode,country,location: {type: 'Point',coordinates: [location.lon, location.lat]},userId:req.userId});
+                return res.status(201).json(address);
+            }else{
+                return res.status(404).json({error:'enter valid address'});
+            }
         }
+        return res.json({message:'Address already created'});
+        
     }catch(error){
         console.log(error);
         return res.status(500).json({error:'Something went wrong'});
@@ -32,11 +38,13 @@ addressController.updateAddress = async(req,res)=>{
     const encodedAddress = encodeURIComponent(`${street} ${city} ${postalCode} ${state} ${country}`);
     const url = `https://api.geoapify.com/v1/geocode/search?text=${encodedAddress}&apiKey=${apiKey}`;
     try{
-        const address = await Address.findByIdAndUpdate(id,{address:location.formatted,street,city,state,postalCode,country,location: {type: 'Point',coordinates: [location.lon, location.lat]}},{new:true});
+        const response = await axios.get(url);
+        const location = response.data.features[0].properties;
+        const address = await Address.findOneAndUpdate({_id:id,userId:req.userId},{address:location.formatted,street,city,state,postalCode,country,location: {type: 'Point',coordinates: [location.lon, location.lat]}},{new:true});
         if(!address){
-            return res.status(404).json({error:'Record not found'});
+            return res.status(404).json({error:'Record not found or unauthorized access'});
         }
-        return res.state(201).json(address);
+        return res.status(201).json(address);
     }catch(error){
         console.log(error);
         return res.status(500).json({error:'Something went wrong'});
