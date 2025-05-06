527981b239991234a2834aff012619c697b028e7
marun-990012 <marun990012@gmail.com>
2025-05-06 22:29:38 +0530

    send email fuction created

---
+++ app/helpers/send-mail.js
@@ -0,0 +1,55 @@
+import nodemailer from 'nodemailer';
+import { transporter } from '../../config/nodemailer.js';
+import { verificationEmailTemplate,welcomeEmailTemplate } from './email-template.js';
+
+export const sendVerificationEamil=(user)=>{
+  
+    const mailOptions = {
+        from: 'arunlamani89@gmail.com',
+        to: user.email,
+        subject: user.message,
+        text: user.message,
+        html:verificationEmailTemplate.replace("{verificationCode}",user.verficationToken)
+    };
+
+    try{
+        transporter.sendMail(mailOptions);
+    }catch(error){
+        console.log(error)
+    }
+};
+
+
+export const senWelcomeEmail=async(user)=>{
+    const mailOptions = {
+        from: 'arunlamani89@gmail.com',
+        to: user.email,
+        subject: user.message,
+        text: user.message,
+        html:welcomeEmailTemplate.replace("{name}",user.name)
+    };
+
+    try{
+        transporter.sendMail(mailOptions);
+    }catch(error){
+        console.log(error)
+    }
+};
+
+
+export const sendResetPasswordEmail = async(data)=>{
+    const mailOptions = {
+        from: 'arunlamani89@gmail.com',
+        to: data.email,
+        subject: 'resete possword',
+        text: data.token,
+        html:welcomeEmailTemplate.replace("{name}",data.token)
+    };
+
+    try{
+        transporter.sendMail(mailOptions);
+    }catch(error){
+        console.log(error)
+    }
+}
+
