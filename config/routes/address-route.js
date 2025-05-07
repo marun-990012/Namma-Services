3546dd271fda6ba0e9dffb9350d71a491d7adc50
marun-990012 <marun990012@gmail.com>
2025-05-07 12:22:34 +0530

    in address route authentication middleware added and validation ares added

--- config/routes/address-route.js
+++ config/routes/address-route.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import { checkSchema } from 'express-validator';
 import authentication from '../../app/middlewares/user-authentication.js';
+import authorization from '../../app/middlewares/user-authorization.js';
 import addressController from '../../app/controllers/address-controller.js';
+import { addressValidation } from '../../app/validators/address-validation-schema.js';
+import { idValidationSchema } from '../../app/validators/id-validation-schema.js';
+import inputValidator from '../../app/helpers/input-validation-helper.js';
+
 
 const addressRoute = express.Router();
 
 //api for address 3
-addressRoute.post('/create',authentication,addressController.createAddress);
-addressRoute.put('/update/:id',authentication,addressController.updateAddress);
+addressRoute.post('/create',authentication,authorization(["work-provider","service-provider"]),checkSchema(addressValidation),inputValidator,addressController.createAddress);
+addressRoute.put('/update/:id',authentication,authorization(["work-provider","service-provider"]),checkSchema(addressValidation),checkSchema(idValidationSchema),inputValidator,addressController.updateAddress);
 addressRoute.get('/find',authentication,addressController.find);
 
 export default addressRoute;
