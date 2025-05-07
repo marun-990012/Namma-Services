9ae7a5347eef56b8fe9b7603cf29b545270e90f9
marun-990012 <marun990012@gmail.com>
2025-05-07 12:36:51 +0530

    validations are added

--- config/routes/wallet-route.js
+++ config/routes/wallet-route.js
@@ -6,7 +6,7 @@ const walletRoute = express.Router();
 
 //api for wallet 3
 walletRoute.post('/create',authentication,walletController.createWallet);
-walletRoute.post('/add/coin',authentication,walletController.addCoin);
+// walletRoute.post('/add/coin',authentication,walletController.addCoin);
 walletRoute.post('/deduct/coin',authentication,walletController.deductCoin);
 walletRoute.get('/find',authentication,walletController.find);
 
