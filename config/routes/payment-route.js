dab7404b7930d50c55c29dd663f4361b405e1bb4
marun-990012 <marun990012@gmail.com>
2025-05-07 12:35:31 +0530

    in payment route validations are applied

--- config/routes/payment-route.js
+++ config/routes/payment-route.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { checkSchema } from 'express-validator';
 import authentication from '../../app/middlewares/user-authentication.js';
 import paymentController from '../../app/controllers/payment-controller.js';
+import { paymentOrderValidation,paymentVerificationValidation } from '../../app/validators/payment-validation-schema.js';
+import inputValidator from '../../app/helpers/input-validation-helper.js';
 
 const paymentRoute = express.Router();
-paymentRoute.post('/order', authentication,paymentController.order);
-paymentRoute.post('/verify',authentication,paymentController.verify);
+paymentRoute.post('/order', authentication,checkSchema(paymentOrderValidation),inputValidator,paymentController.order);
+paymentRoute.post('/verify',authentication,checkSchema(paymentVerificationValidation),inputValidator,paymentController.verify);
 
 export default paymentRoute;
