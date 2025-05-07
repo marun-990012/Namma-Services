2cae7c65d0cdc2d223848ec3df046c6d70338063
marun-990012 <marun990012@gmail.com>
2025-05-07 17:14:09 +0530

    vite app created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="border p-6 bg-black">
+        <p className="text-white">welcome to application</p>
+      </div>
+    </>
+  )
+}
+
+export default App
