600ae4757c19f1a2f401022ed23473296e2e4dcb
marun-990012 <marun990012@gmail.com>
2025-05-07 12:36:22 +0530

    in review route schema validations are added

--- config/routes/review-route.js
+++ config/routes/review-route.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import authentication from '../../app/middlewares/user-authentication.js';
+import authorization from '../../app/middlewares/user-authorization.js';
 import reviewRatingController from '../../app/controllers/rating-controller.js';
+import { reviewRatingValidation,moreReviewValidation,updateReviewValidation } from '../../app/validators/review-rating-validation-schema.js';
+import { idValidationSchema,reviewIdValidation } from '../../app/validators/id-validation-schema.js';
+import inputValidator from '../../app/helpers/input-validation-helper.js';
+import { checkSchema } from 'express-validator';
 
 const reviewRoute = express.Router();
 
-reviewRoute.post('/create/',reviewRatingController.create);
-reviewRoute.post('/add/:id',reviewRatingController.moreReview);
-reviewRoute.delete('/delete/:id/:reviewid',reviewRatingController.removeReview);
-reviewRoute.put('/update/:id/:reviewid',reviewRatingController.updateReview);
+reviewRoute.post('/create/',authentication,authorization(['work-provider']),checkSchema(reviewRatingValidation),inputValidator,reviewRatingController.create);
+reviewRoute.post('/add/:id',authentication,authorization(['work-provider']),checkSchema(moreReviewValidation),checkSchema(idValidationSchema),inputValidator,reviewRatingController.moreReview);
+reviewRoute.delete('/delete/:id/:reviewid',authentication,checkSchema(reviewIdValidation),inputValidator,reviewRatingController.removeReview);
+reviewRoute.put('/update/:id/:reviewid',authentication,authorization(['work-provider']),checkSchema(updateReviewValidation),checkSchema(idValidationSchema),inputValidator,reviewRatingController.updateReview);
+reviewRoute.get('/list',authentication,reviewRatingController.list);
 
 export default reviewRoute;
