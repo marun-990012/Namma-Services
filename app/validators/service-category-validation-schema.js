f3b2604a07da51617a0ece0fdcbc42db025d8b75
marun-990012 <marun990012@gmail.com>
2025-05-07 12:19:58 +0530

    service category schema validation file modified

--- app/validators/service-category-validation-schema.js
+++ app/validators/service-category-validation-schema.js
@@ -1,7 +1,7 @@
 import Service from "../models/service-category-model.js"
 
 //Service category validation schema
-export const serviceCategoryValidationSchema = {
+export const serviceCategoryValidation = {
 
     //validation for name field
     name:{
@@ -24,6 +24,47 @@ export const serviceCategoryValidationSchema = {
                     throw new Error(error.message);
                 }
                 return true;
+            }
+        }
+    },
+
+    //validation for description field
+    description:{
+        in:['body'],
+        exists:{
+            errorMessage:'description field is required'
+        },
+        notEmpty:{
+            errorMessage:'description field should not be empty'
+        }
+    }
+
+};
+
+
+export const serviceUpdateValidation = {
+    //validation for name field
+    name:{
+        in:['body'],
+        exists:{
+            errorMessage:'name field is required'
+        },
+        notEmpty:{
+            errorMessage:'name field should not be empty'
+        },
+        trim:true,
+        custom:{
+            options: async function (value,{req}) {
+                try{
+                    const service = await Service.findOne({name:value});
+                    if(service && service._id.toString() !== req.params.id){
+                        throw new Error('Category already exists');
+                    }  
+                }catch(error){
+                    throw new Error(error.message);
+                }
+                return true
+                
             }
         }
     },
