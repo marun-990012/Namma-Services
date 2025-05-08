a241024ab34ad78bf7e49ce9f4942684045612b2
marun-990012 <marun990012@gmail.com>
2025-05-08 23:30:37 +0530

    cloudinary config file updated

--- backend/config/cloudinaryConfig.js
+++ backend/config/cloudinaryConfig.js
@@ -1,8 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
-cloudinary.config({ 
-    cloud_name: 'dxludspye', 
-    api_key: '594237119177773', 
-    api_secret: 'TiogsJ8kmhz5Fm48y609f3m8Rrc' // Click 'View API Keys' above to copy your API secret
-});
-
-export default cloudinary;
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' }); 
+cloudinary.config({
+    cloud_name: 'dxludspye',
+    api_key: "594237119177773",
+    api_secret: "TiogsJ8kmhz5Fm48y609f3m8Rrc",
+  });
+  
+  export default cloudinary;
