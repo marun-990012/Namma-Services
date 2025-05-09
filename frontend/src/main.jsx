d98df066b5f6cf137377ec7c7e85f67253487906
marun-990012 <marun990012@gmail.com>
2025-05-09 23:44:55 +0530

    in main.jsx file react hot toast added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import { Toaster } from 'react-hot-toast';
 import './index.css'
 import App from './App.jsx'
+import store from './redux/store/store.jsx';
 
 createRoot(document.getElementById('root')).render(
- 
+  <Provider store={store}>
+    <BrowserRouter>
     <App />
+    <Toaster position="bottom-right" reverseOrder={false} />
+  </BrowserRouter>
+  </Provider>
   
 )
