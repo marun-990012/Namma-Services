9f15c5c8bd7fb3ca3d2733c220bf6a54ad30326f
marun-990012 <marun990012@gmail.com>
2025-05-09 23:32:42 +0530

    user model updated

--- backend/app/models/user-model.js
+++ backend/app/models/user-model.js
@@ -28,6 +28,7 @@ const userSchema=new Schema({
         type:String
     },
     verificationToken:String,
+    tokenExpires :String,
     passwordResetToken:String,
     passwordResetExpires:String
     
