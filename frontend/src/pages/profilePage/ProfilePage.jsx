22fc37b8d67f506b0cc9fe48fbe7dbd6f78d1100
marun-990012 <marun990012@gmail.com>
2025-05-10 23:06:11 +0530

    Profile Page created

---
+++ frontend/src/pages/profilePage/ProfilePage.jsx
@@ -0,0 +1,101 @@
+import { Star,BadgeCheck,ImagePlus   } from "lucide-react";
+function ProfilePage() {
+  return (
+    <div>
+      <div className="flex justify-center border-5 border-white rounded-[10px] shadow-[8px]">
+        <div className=" w-full rounded-tl-[10px] rounded-tr-[10px]">
+            {/* banner image div */}
+          <div>
+            <img
+              className="rounded-tl-[10px] rounded-tr-[10px] w-full h-50"
+              src="https://res.cloudinary.com/dxludspye/image/upload/v1746890819/Namma-Services/ugqpmgx9texs3ybispn9.png"
+              alt=""
+            />
+          </div>
+
+          {/* profile div */}
+          <div className="flex justify-between p-5">
+            <div className="flex justify-evenly w-[52%]">
+              <div className="mt-[-75px] p-2 bg-[#e3e7ef] rounded-full">
+                <img
+                  className="w-40 rounded-full"
+                  src="https://res.cloudinary.com/dxludspye/image/upload/v1746891160/Namma-Services/fvcjr2ts3hvs9d7dod64.png"
+                  alt=""
+                />
+              </div>
+              <div className="mt-[-13px]">
+                <p className="text-[25px] flex items-center">Arun Rathod < BadgeCheck color="#06f"  className="ml-2" /></p>
+                <p className="text-gray-600">Service Provider - electrician</p>
+                <p className="text-gray-800">
+                  Arun Rathod Service Provider Service Provider
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <p>Social Media</p>
+            </div>
+          </div>
+
+          {/* review and rating div */}
+          <div className="flex justify-center p-4">
+            <div className=" mt-1 flex justify-evenly h-50 w-[98%]">
+              <div className="bg-white w-112 p-4 rounded-[8px]">
+                <p className="text-[17px] font-medium">Review and Ratings</p>
+                <div className="mt-2 flex justify-between mr-7 ">
+                  <div>
+                    <p className="flex items-center font-medium text-[35px]">
+                      {[...Array(5)].map((_, i) => (
+                        <Star key={i} color="#ecd909" />
+                      ))}
+                      <span className="ml-2">4.5</span>
+                    </p>
+                  </div>
+                   <div>
+                    <p>
+                      <span className="text-[17px]">All reviews</span> - <span className="text-[35px] font-medium">123</span>
+                    </p>
+                   </div>
+                </div>
+                <div className="text-center mt-9">
+                    <button className="bg-orange-400 hover:bg-orange-600 cursor-pointer w-75 py-2 rounded text-[17px] text-white">View all reviews</button>
+                </div>
+              </div>
+
+              <div className="bg-white w-112 p-4 rounded-[8px]">
+                <p className="text-[17px] font-medium">Total work completed</p>
+                <div className="mt-2 flex justify-center mr-7 ">
+                  
+                   <div>
+                    <p>
+                      <span className="text-[17px]">Total work</span> - <span className="text-[35px] font-medium">304</span>
+                    </p>
+                   </div>
+                </div>
+                <div className="text-center mt-9">
+                    <button className="bg-orange-400 hover:bg-orange-600 cursor-pointer w-75 py-2 rounded text-[17px] text-white">View completed works</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className=" mb-4 mt-3 mx-20 ">
+            <p className="text-[17px] font-medium">Work releted images</p>
+            <div className="mt-3 p-2 rounded bg-white flex justify-evenly">
+                <img className="w-45 rounded border" src="https://res.cloudinary.com/dxludspye/image/upload/v1746891160/Namma-Services/fvcjr2ts3hvs9d7dod64.png" alt="" />
+                <img className="w-45 rounded" src="https://res.cloudinary.com/dxludspye/image/upload/v1746891160/Namma-Services/fvcjr2ts3hvs9d7dod64.png" alt="" />
+                <img className="w-45 rounded" src="https://res.cloudinary.com/dxludspye/image/upload/v1746891160/Namma-Services/fvcjr2ts3hvs9d7dod64.png" alt="" />
+                <img className="w-45 rounded" src="https://res.cloudinary.com/dxludspye/image/upload/v1746891160/Namma-Services/fvcjr2ts3hvs9d7dod64.png" alt="" />
+                {/* <img className="w-45 rounded" src="https://res.cloudinary.com/dxludspye/image/upload/v1746891160/Namma-Services/fvcjr2ts3hvs9d7dod64.png" alt="" /> */}
+                <div className="w-45 h-45 flex items-center justify-center border border-[#989695] rounded">
+                    <p><ImagePlus size={30} color="#989695"/></p>
+                </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ProfilePage;
