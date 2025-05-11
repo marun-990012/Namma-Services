c56f93a176f20bc9ad1e246288b9649a43316573
marun-990012 <marun990012@gmail.com>
2025-05-11 23:14:04 +0530

    new reduces added in store

--- frontend/src/redux/store/store.jsx
+++ frontend/src/redux/store/store.jsx
@@ -1,9 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../slices/authSlice';
+import serviceCategoryReducer from '../slices/serviceCategorySlice';
+import profileReducer from '../slices/profileSlice'
+
 
 const store = configureStore({
     reducer:{
-        auth:authReducer
+        auth:authReducer,
+        services:serviceCategoryReducer,
+        profile:profileReducer
     }
 });
 
