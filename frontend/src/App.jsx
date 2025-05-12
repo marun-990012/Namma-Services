d809257446aa0a38a4c08936f0629d7ac2962908
marun-990012 <marun990012@gmail.com>
2025-05-12 23:50:29 +0530

    in app.js file new routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes,Route,Link } from "react-router-dom";
 import MainLayout from "./pages/layout/MainLayout";
 import AuthPage from "./pages/user-auth/AuthPage";
 import EmailVerification from "./pages/user-auth/EmailVerification";
+import CategoryForm from "./pages/service-category/CategoryForm";
 import CategoryList from "./pages/service-category/CategoryList";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 
@@ -14,11 +15,13 @@ function App() {
         <Routes>
           <Route element={<MainLayout/>}>
 
-          <Route path="/login" element={<AuthPage/>}/>
-          <Route path="/register" element={<AuthPage/>}/>
-          <Route path="/email-verification/:id" element={<EmailVerification/>}/>
-          <Route path="/service-category" element={<CategoryList/>}/>
-          <Route path="/profile" element={<ProfilePage/>}/>
+            <Route path="/login" element={<AuthPage/>}/>
+            <Route path="/register" element={<AuthPage/>}/>
+            <Route path="/email-verification/:id" element={<EmailVerification/>}/>
+            <Route path="/category/new" element={<CategoryForm/>}/>
+            <Route path="/category/edit/:id" element={<CategoryForm/>}/>
+            <Route path="/service-category" element={<CategoryList/>}/>
+            <Route path="/profile" element={<ProfilePage/>}/>
 
           </Route>
           
