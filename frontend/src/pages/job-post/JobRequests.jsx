b7cc4a7f759dde0bf521b7b2a73f4d21262c7c3b
marun-990012 <marun990012@gmail.com>
2025-05-17 22:59:23 +0530

    in Job Request page changed some styling

--- frontend/src/pages/job-post/JobRequests.jsx
+++ frontend/src/pages/job-post/JobRequests.jsx
@@ -1,24 +1,67 @@
-function JobRequests(){
+import { useEffect,useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { showJobPostDetail } from "../../redux/slices/jobPostSlice";
+
+function JobRequests({id}){
+   const dispatch = useDispatch();
+   useEffect(() => {
+    dispatch(showJobPostDetail(id));
+  }, [dispatch]);
+
+  const jobPost = useSelector((state) => {
+    return state.jobs;
+  }).data;
+  console.log(jobPost);
     return(
-        <div className="mt-5">
-            {[1,2,3,4,5].map(()=>{
-                return(
-                    <div className="flex justify-evenly items-center bg-gray-200 py-1 rounded mt-3">
-                <div className="bg-green-300 w-16 h-16 flex justify-center items-center rounded-full">
-                    <img className="w-15 h-15 rounded-full" src="https://res.cloudinary.com/dxludspye/image/upload/v1747306608/Namma-Services/rpkkgdua0dnhpt2hwstr.jpg" alt="" />
-                </div>
-                <div className="w-30  ml-[-25px]">
-                    <p>Arun Rathod</p>
-                </div>
-                 <div className="w-70">
-                    <p>Arun Rathod Arun Rathod Arun Rathod Arun Rathod Arun Rathod Arun Rathod</p>
-                 </div>
-                <p>Arun Rathod</p>
-                <button className="cursor-pointer bg-green-300 text-green-800 hover:bg-green-400 hover:text-black px-5 py-1 rounded">View</button>
-            </div>
-                )
-            })}
+        <div className="mt-5 space-y-3">
+  {jobPost.jobRequests?.map((_, index) => (
+    <div
+      key={index}
+      className="flex flex-col sm:flex-row sm:items-center bg-gray-200 p-2 rounded gap-4"
+    >
+      {/* Profile Image */}
+      <div className="flex justify-center sm:justify-start">
+        <div className="bg-green-300 w-16 h-16 flex justify-center items-center rounded-full overflow-hidden">
+          <img
+            className="w-full h-full object-cover rounded-full"
+            src="https://res.cloudinary.com/dxludspye/image/upload/v1747306608/Namma-Services/rpkkgdua0dnhpt2hwstr.jpg"
+            alt="Profile"
+          />
         </div>
+      </div>
+
+      {/* Text Info Block */}
+      <div className="flex-1 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2 text-center sm:text-left">
+        {/* Name */}
+        <div className="sm:w-1/4">
+          <p className="font-semibold">Arun Rathod</p>
+        </div>
+
+        {/* Description */}
+        <div className="sm:w-2/4">
+          <p className="truncate sm:whitespace-normal">
+            Arun Rathod Arun Rathod Arun Rathod Arun Rathod Arun Rathod Arun Rathod
+          </p>
+        </div>
+
+        {/* Rating */}
+        <div className="sm:w-1/4">
+          <p className="font-medium">Rating 4.5</p>
+        </div>
+      </div>
+
+      {/* Button */}
+      <div className="text-center sm:text-right mt-2 sm:mt-0">
+        <button className="cursor-pointer bg-green-300 text-green-800 hover:bg-green-400 hover:text-black px-5 py-1 rounded">
+          View
+        </button>
+      </div>
+    </div>
+  ))}
+</div>
+
+
     )
 }
 export default JobRequests;
