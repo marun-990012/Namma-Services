07b917c13eefac5d098b3f66f8dc7851c66949b9
marun-990012 <marun990012@gmail.com>
2025-05-19 23:17:33 +0530

    jobPostSlice modified

--- frontend/src/redux/slices/jobPostSlice.jsx
+++ frontend/src/redux/slices/jobPostSlice.jsx
@@ -71,12 +71,40 @@ export const sendJobRequest = createAsyncThunk('/jobs/sendJobRequest',async({id,
     }
 });
 
-
-export const selectServiceProvider = createAsyncThunk('/jobs/selectServiceProvider',async({id,selectedServiceProviderId},{rejectWithValue})=>{
+export const considerServiceProvider = createAsyncThunk('/jobs/considerServiceProvider',async({id,serviceProviderId},{rejectWithValue})=>{
     // console.log('thunk',id)
     // console.log('thunk',serviceProviderIds)
     try{
+        const response = await axiosInstance.post(`/job/consideration/${id}`,{serviceProvider:serviceProviderId},{headers:{
+            Authorization:localStorage.getItem('token')
+        }});
+        console.log(response.data);
+        return response.data;
+    }catch(error){
+        console.log(error);
+        return rejectWithValue(error?.response?.data);
+
+    }
+});
+
+export const selectServiceProvider = createAsyncThunk('/jobs/selectServiceProvider',async({id,selectedServiceProviderId},{rejectWithValue})=>{
+    try{
         const response = await axiosInstance.post(`/job/select/${id}`,{serviceProvider:selectedServiceProviderId},{headers:{
+            Authorization:localStorage.getItem('token')
+        }});
+        console.log(response.data);
+        return response.data;
+    }catch(error){
+        console.log(error);
+        return rejectWithValue(error?.response?.data);
+
+    }
+});
+
+export const replyToServiceProvider = createAsyncThunk('/jobs/replyToServiceProvider',async({id,formData},{rejectWithValue})=>{
+    console.log(formData)
+    try{
+        const response = await axiosInstance.post(`/job/reply/${id}`,formData,{headers:{
             Authorization:localStorage.getItem('token')
         }});
         console.log(response.data);
@@ -192,6 +220,30 @@ const jobPostSlice = createSlice({
         })
 
 
+        //consider service provider
+        .addCase(considerServiceProvider.pending,(state,action)=>{
+             state.loading = true;
+             state.error = null;
+        })
+        
+        .addCase(considerServiceProvider.fulfilled,(state,action)=>{
+             const index = state.data.findIndex((post)=>{
+             return post._id == action.payload._id;
+             });
+             if (index !== -1) {
+             state.data[index] = action.payload;
+             }
+             state.job = action.payload;
+             state.loading = false;
+             state.error = null;
+        })
+        
+        .addCase(considerServiceProvider.rejected,(state,action)=>{
+             state.loading = false;
+             state.error = action.payload;
+        })
+
+
         //select service provider
         .addCase(selectServiceProvider.pending,(state,action)=>{
              state.loading = true;
@@ -211,6 +263,30 @@ const jobPostSlice = createSlice({
         })
         
         .addCase(selectServiceProvider.rejected,(state,action)=>{
+             state.loading = false;
+             state.error = action.payload;
+        })
+
+
+        //reply to service provider
+        .addCase(replyToServiceProvider.pending,(state,action)=>{
+             state.loading = true;
+             state.error = null;
+        })
+        
+        .addCase(replyToServiceProvider.fulfilled,(state,action)=>{
+             const index = state.data.findIndex((post)=>{
+             return post._id == action.payload._id;
+             });
+             if (index !== -1) {
+             state.data[index] = action.payload;
+             }
+             state.job = action.payload;
+             state.loading = false;
+             state.error = null;
+        })
+        
+        .addCase(replyToServiceProvider.rejected,(state,action)=>{
              state.loading = false;
              state.error = action.payload;
         })
