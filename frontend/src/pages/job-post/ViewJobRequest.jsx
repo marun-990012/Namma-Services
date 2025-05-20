f166803995ac981ad6b73cf79e94392cf029744e
marun-990012 <marun990012@gmail.com>
2025-05-20 23:21:15 +0530

    in view job request page dynamic data added

--- frontend/src/pages/job-post/ViewJobRequest.jsx
+++ frontend/src/pages/job-post/ViewJobRequest.jsx
@@ -1,25 +1,24 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  BadgeCheck,
-  Star,
-  MessageSquareShare,
-  SendHorizontal,
-} from "lucide-react";
-import { Link } from "react-router-dom";
+import {BadgeCheck,Star,MessageSquareShare,SendHorizontal,} from "lucide-react";
+import { Link,useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
-import {
-  showJobPostDetail,
-  replyToServiceProvider,
-} from "../../redux/slices/jobPostSlice";
+import {showJobPostDetail,replyToServiceProvider,} from "../../redux/slices/jobPostSlice";
 import { fetchServiceProviders } from "../../redux/slices/userSlice";
 import { fetchAccount } from "../../redux/slices/profileSlice";
+import { useSelectServiceProvider } from "../../hooks/useSelectServiceProvider";
+import { useConsiderServiceProvider } from "../../hooks/useConsiderServiceProvider";
 function ViewJobRequest() {
   const { userId, id } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {select} = useSelectServiceProvider();
+  const {consider} = useConsiderServiceProvider();
+
 
   const [replyMessage, setReplyMessage] = useState("");
+   const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     dispatch(showJobPostDetail(id));
@@ -59,6 +58,26 @@ function ViewJobRequest() {
     }
   };
 
+  const handleConsider = async()=>{
+    const formData = {id,serviceProviderId:userId}
+    consider(formData)
+  }
+
+const handleSelect = (serviceProviderId) => {
+  // setSelectedServiceProviderId(serviceProviderId); // store ID
+  setShowPopup(true); // show confirmation popup
+};
+
+ const handleConfirm = async() => {
+    setShowPopup(false);
+    const formData = {id,selectedServiceProviderId:userId}
+    select(formData)
+  };
+  
+ const handleCancel = () => {
+    setShowPopup(false);
+  };
+
   return (
     <div className="flex justify-center items-center border-3 border-white p-10 pt-4 rounded-[8px] mb-2">
       <div className="w-[90%]">
@@ -75,10 +94,10 @@ function ViewJobRequest() {
           </div>
 
           <div className="flex gap-2">
-            <button className="cursor-pointer px-4 py-[2px] bg-[#facc15] hover:bg-[#edc10f] rounded-[6px] outline-none">
+            <button onClick={()=>{handleConsider()}} className="cursor-pointer px-4 py-[2px] bg-[#facc15] hover:bg-[#edc10f] rounded-[6px] outline-none">
               Consider
             </button>
-            <button onClick={()=>{}} className="cursor-pointer px-4 py-[2px] bg-[#22c55e] hover:bg-[#1cb856] text-white rounded-[6px] outline-none">
+            <button onClick={()=>{handleSelect()}} className="cursor-pointer px-4 py-[2px] bg-[#22c55e] hover:bg-[#1cb856] text-white rounded-[6px] outline-none">
               Select
             </button>
           </div>
@@ -194,6 +213,35 @@ function ViewJobRequest() {
           </div>
         </div>
       </div>
+
+      {/* show popup */}
+ {showPopup && (
+  <div className="fixed inset-0 z-50 flex justify-center items-center bg-black/30 backdrop-blur-sm">
+    <div className="bg-white text-gray-800 p-6 rounded-2xl shadow-2xl max-w-md w-full mx-4 border border-purple-200">
+      <h2 className="text-lg font-semibold text-purple-700 mb-3">
+        Confirm Selection
+      </h2>
+      <p className="text-sm sm:text-base mb-6 leading-relaxed text-gray-600">
+        Are you sure you want to assign this service provider to the job?
+      </p>
+
+      <div className="flex justify-end gap-3">
+        <button
+          onClick={handleCancel}
+          className="px-4 py-2 text-sm font-medium rounded-md bg-gray-100 text-gray-700 hover:bg-gray-200 transition"
+        >
+          Cancel
+        </button>
+        <button
+          onClick={handleConfirm}
+          className="px-4 py-2 text-sm font-semibold rounded-md bg-yellow-400 text-purple-800 hover:bg-yellow-500 transition"
+        >
+          Confirm
+        </button>
+      </div>
+    </div>
+  </div>
+)}
     </div>
   );
 }
