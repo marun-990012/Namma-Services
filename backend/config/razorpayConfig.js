81ddc0ed11ac8de71ed293d1c20e436903efe173
marun-990012 <marun990012@gmail.com>
2025-05-21 23:46:51 +0530

    razorpay config file modified

--- backend/config/razorpayConfig.js
+++ backend/config/razorpayConfig.js
@@ -1,6 +1,6 @@
 
 import Razorpay from "razorpay";
 export const razorpayInstance = new Razorpay({
-    key_id: "rzp_test_cVi012fWsvhHkY",          // ✅ your Razorpay KEY ID (starts with rzp_test_)
-    key_secret: "nvv3jxWSqIrXxm2SbB2AlOIZ"      // ✅ your Razorpay SECRET
+    key_id: "rzp_test_d8jU7S0YhIAw1x",          // ✅ your Razorpay KEY ID (starts with rzp_test_)
+    key_secret: "5DrtcuKEVGGvv0L57aPiAi8A"      // ✅ your Razorpay SECRET
   });
