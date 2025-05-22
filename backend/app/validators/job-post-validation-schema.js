c63d752c0e7bcd70b24b71c047eea485eb82d9cf
marun-990012 <marun990012@gmail.com>
2025-05-22 23:06:45 +0530

    job post schema validation modified

--- backend/app/validators/job-post-validation-schema.js
+++ backend/app/validators/job-post-validation-schema.js
@@ -92,18 +92,18 @@ export const jobRequestValidation = {
 
 
   export const sendMessageValidation = {
-    requestId: {
-        in: ['body'],
-        exists: {
-            errorMessage: 'requestId field is required',
-        },
-        notEmpty: {
-            errorMessage: 'requestId field should not be empty',
-        },
-        isMongoId: {
-            errorMessage: 'Invalid requestId format',
-        }
-    },
+    // requestId: {
+    //     in: ['body'],
+    //     exists: {
+    //         errorMessage: 'requestId field is required',
+    //     },
+    //     notEmpty: {
+    //         errorMessage: 'requestId field should not be empty',
+    //     },
+    //     isMongoId: {
+    //         errorMessage: 'Invalid requestId format',
+    //     }
+    // },
     message: {
         in: ['body'],
         exists: {
