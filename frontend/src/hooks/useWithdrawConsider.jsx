59b732b35f286400635a95fe95e39af241b214dd
marun-990012 <marun990012@gmail.com>
2025-05-22 23:18:38 +0530

    WithDraw considerations custome hook created

---
+++ frontend/src/hooks/useWithdrawConsider.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { withdrawConsider } from "../redux/slices/jobPostSlice";
+import toast from "react-hot-toast";
+
+export const usewithdrawConsider = () => {
+  const dispatch = useDispatch();
+
+  const withdraw = async (formData) => {
+    console.log(formData)
+    try {
+      const res = await dispatch(
+        withdrawConsider(formData)
+      ).unwrap();
+      toast.success(" successfully withdraw consider");
+      return res;
+    } catch (error) {
+      toast.error(error.message || "Failed to withdraw consider. Please try again.");
+      throw error;
+    }
+  };
+
+  return { withdraw };
+};
