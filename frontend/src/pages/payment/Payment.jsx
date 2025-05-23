9f159783a4d09f0b6bbb2519a967eb2e027bf2bf
marun-990012 <marun990012@gmail.com>
2025-05-23 23:47:53 +0530

    payment page modified

--- frontend/src/pages/payment/Payment.jsx
+++ frontend/src/pages/payment/Payment.jsx
@@ -1,8 +1,9 @@
 import { Coins } from "lucide-react";
 import Wallet from "./Wallet";
+import { useLocation } from "react-router-dom";
 import Transactions from "./Transactions";
 function Payment() {
-
+    
   return (
     <div className="flex justify-center items-center border-3 border-white shodow-xl rounded-[8px] p-10 mb-4">
       <div className="flex w-full justify-between">
