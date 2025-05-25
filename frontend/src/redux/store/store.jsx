d78953181b647bb7f7a3521e70095969eb138310
marun-990012 <marun990012@gmail.com>
2025-05-25 23:59:52 +0530

    Payment Reducer added in redux store

--- frontend/src/redux/store/store.jsx
+++ frontend/src/redux/store/store.jsx
@@ -7,6 +7,7 @@ import addressReducer from '../slices/profileAddressSlice';
 import jobPostReducer from '../slices/jobPostSlice';
 import userReducer from '../slices/userSlice';
 import walletReducer from '../slices/WalletSlice';
+import paymentReducer from '../slices/paymentSlice';
 import reviewRatingReducer from '../slices/reviewRatingSlice';
 
 
@@ -20,6 +21,7 @@ const store = configureStore({
         jobs:jobPostReducer,
         users:userReducer,
         wallet:walletReducer,
+        payment:paymentReducer,
         review:reviewRatingReducer
     }
 });
