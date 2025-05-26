e369385a7ef0156cd6d3f22575867741bf3d78ea
marun-990012 <marun990012@gmail.com>
2025-05-26 23:21:26 +0530

    new toolTip message added in viewJoPostRequest file

--- frontend/src/pages/job-post/ViewJobRequest.jsx
+++ frontend/src/pages/job-post/ViewJobRequest.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState ,useRef} from "react";
+import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   BadgeCheck,
@@ -29,8 +29,7 @@ function ViewJobRequest() {
 
   const [replyMessage, setReplyMessage] = useState("");
   const [showPopup, setShowPopup] = useState(false);
-   const messagesEndRef = useRef(null);
-
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     dispatch(showJobPostDetail(id));
@@ -52,15 +51,16 @@ function ViewJobRequest() {
     return message.serviceProvider == userId;
   });
 
-  console.log(requestMessages?.messages);
-
+  // console.log(jobPost.job.selectedServiceProvider == userId);
+  
   const isConsidered = jobPost?.job?.considerations?.includes(userId);
+  const isSameServiceProvider = jobPost.job.selectedServiceProvider == userId;
   // console.log(isConsidered);
 
   useEffect(() => {
-      // Scroll to bottom when messages update
-      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, [requestMessages]);
+    // Scroll to bottom when messages update
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [requestMessages]);
 
   const handleSendReply = async (e) => {
     e.preventDefault();
@@ -105,7 +105,7 @@ function ViewJobRequest() {
   const handleCancel = () => {
     setShowPopup(false);
   };
-
+  console.log(jobPost.job.workStatus);
   return (
     <div className="flex justify-center items-center border-3 border-white p-10 pt-4 rounded-[8px] mb-2">
       <div className="w-full">
@@ -122,79 +122,122 @@ function ViewJobRequest() {
           </div>
 
           <div className="flex gap-2">
-            {isConsidered ? (
-              <button
-                onClick={() => handleWithdrawConsider()}
-                className="flex items-center gap-2 px-4 py-2 bg-red-100 text-red-600 hover:bg-red-200 hover:text-red-700 border border-red-300 rounded-xl transition duration-200 shadow-sm hover:shadow-md focus:outline-none focus:ring-2 focus:ring-red-300"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-                Withdraw Consideration
-              </button>
-            ) : (
-              <div className="relative group inline-block">
-                {/* Button */}
-                <button
-                  disabled={jobPost.isWorking}
-                  onClick={() => handleConsider()}
-                  className={`flex items-center gap-2 px-5 py-2 font-medium rounded-lg shadow-sm transition duration-200 ease-in-out focus:outline-none focus:ring-2
-             ${
-              jobPost.isWorking
-               ? "bg-gray-300 text-gray-600 cursor-not-allowed focus:ring-gray-300"
-               : "bg-yellow-400 hover:bg-yellow-500 text-black focus:ring-yellow-300"
-            }`} 
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
+            {jobPost.job.workStatus !== "completed" ? (
+              <>
+                {isConsidered ? (
+                  <button
+                    onClick={() => handleWithdrawConsider()}
+                    className="flex items-center gap-2 px-4 py-2 bg-red-100 text-red-600 hover:bg-red-200 hover:text-red-700 border border-red-300 rounded-xl transition duration-200 shadow-sm hover:shadow-md focus:outline-none focus:ring-2 focus:ring-red-300"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9 17v-6h13M5 17H3v-6h2m0 6V9.5a1.5 1.5 0 011.5-1.5H7"
-                    />
-                  </svg>
-                  Consider
-                </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                    Withdraw Consideration
+                  </button>
+                ) : (
+                  <div className="relative group inline-block">
+                    {/* Button */}
+                    <button
+                      disabled={jobPost.isWorking}
+                      onClick={() => handleConsider()}
+                      className={`flex items-center gap-2 px-5 py-2 font-medium rounded-lg shadow-sm transition duration-200 ease-in-out focus:outline-none focus:ring-2
+             ${
+               jobPost.isWorking
+                 ? "bg-gray-300 text-gray-600 cursor-not-allowed focus:ring-gray-300"
+                 : "bg-yellow-400 hover:bg-yellow-500 text-black focus:ring-yellow-300"
+             }`}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 17v-6h13M5 17H3v-6h2m0 6V9.5a1.5 1.5 0 011.5-1.5H7"
+                        />
+                      </svg>
+                      Consider
+                    </button>
 
-                {/* Tooltip */}
-                {jobPost.isWorking && (
-                  <div className="absolute top-full mt-2 left-1/2 transform -translate-x-1/2 bg-gray-800 text-gray-100 text-xs rounded-md shadow-lg py-1.5 px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap border border-gray-700">
-                    User is already working on another job
+                    {!isSameServiceProvider ? (
+                      <>
+                      {/* Tooltip */}
+                    {jobPost.isWorking && (
+                      <div className="absolute top-full mt-2 left-1/2 transform -translate-x-1/2 bg-gray-800 text-gray-100 text-xs rounded-md shadow-lg py-1.5 px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap border border-gray-700">
+                        User is already working on another job
+                      </div>
+                    )}
+                      </>
+                    ):(
+                      <>
+                      {/* Tooltip */}
+                    {jobPost.isWorking && (
+                      <div className="absolute top-full mt-2 left-1/2 transform -translate-x-1/2 bg-gray-800 text-gray-100 text-xs rounded-md shadow-lg py-1.5 px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap border border-gray-700">
+                        This job is already assigned to this user.
+                      </div>
+                    )}
+                      </>
+                    )}
+                    
                   </div>
                 )}
-              </div>
-            )}
 
-            <div className="relative group inline-block">
-              <button
-                disabled={!!jobPost.isWorking}
-                onClick={handleSelect}
-                className={`flex items-center gap-2 px-5 py-2 font-medium rounded-lg shadow-sm transition duration-200 ease-in-out focus:outline-none focus:ring-2 ${
-                  jobPost.isWorking
-                    ? "bg-gray-400 text-white cursor-not-allowed focus:ring-gray-300"
-                    : "bg-green-500 hover:bg-green-600 text-white focus:ring-green-400"
-                }`}
-              >
+                <div className="relative group inline-block">
+                  {!jobPost.job.selectedServiceProvider? (
+                    <>
+                    <button
+                    disabled={!!jobPost.isWorking}
+                    onClick={handleSelect}
+                    className={`flex items-center gap-2 px-5 py-2 font-medium rounded-lg shadow-sm transition duration-200 ease-in-out focus:outline-none focus:ring-2 ${
+                      jobPost.isWorking
+                        ? "bg-gray-400 text-white cursor-not-allowed focus:ring-gray-300"
+                        : "bg-green-500 hover:bg-green-600 text-white focus:ring-green-400"
+                    }`}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M5 13l4 4L19 7"
+                      />
+                    </svg>
+                    Select
+                  </button>
+
+                  {jobPost.isWorking && (
+                    <div className="absolute top-full mt-2 left-1/2 transform -translate-x-1/2 bg-gray-800 text-gray-100 text-xs rounded-md shadow-lg py-1.5 px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap border border-gray-700">
+                      User is already working on another job
+                    </div>
+                  )}
+                    </>
+                  ) :(
+                    <div className="flex items-center gap-3 px-5 py-3 bg-green-100 text-green-800 border border-green-300 rounded-xl shadow-sm">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
+                  className="h-5 w-5 text-green-700"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -206,15 +249,34 @@ function ViewJobRequest() {
                     d="M5 13l4 4L19 7"
                   />
                 </svg>
-                Select
-              </button>
-
-              {jobPost.isWorking && (
-                <div className="absolute top-full mt-2 left-1/2 transform -translate-x-1/2 bg-gray-800 text-gray-100 text-xs rounded-md shadow-lg py-1.5 px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap border border-gray-700">
-                  User is already working on another job
+                <p className="text-sm font-medium">
+                  Service provier selected.
+                </p>
+              </div>
+                  )}
                 </div>
-              )}
-            </div>
+              </>
+            ) : (
+              <div className="flex items-center gap-3 px-5 py-3 bg-green-100 text-green-800 border border-green-300 rounded-xl shadow-sm">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-green-700"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <p className="text-sm font-medium">
+                  This work has been successfully completed.
+                </p>
+              </div>
+            )}
           </div>
         </div>
 
