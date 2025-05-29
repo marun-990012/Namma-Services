3fad490c2af35b149074547348fa6b39102d92c3
marun-990012 <marun990012@gmail.com>
2025-05-29 23:07:19 +0530

    in review slice created review thunk added

--- frontend/src/redux/slices/reviewRatingSlice.jsx
+++ frontend/src/redux/slices/reviewRatingSlice.jsx
@@ -8,7 +8,32 @@ export const createReview = createAsyncThunk('/reviews/createReview',async(formD
         return response.data;
     } catch (error) {
         console.log(error);
-        return rejectWithValue(error?.response?.data || { message : "error while fetching address"})
+        return rejectWithValue(error?.response?.data || { message : "error while create review"})
+    }
+});
+
+
+
+export const fetchReviews = createAsyncThunk('/reviews/fetchReviews',async(id,{rejectWithValue})=>{
+    try {
+        const response = await axiosInstance.get(`/review/list/${id}`,{headers:{Authorization:localStorage.getItem('token')}});
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return rejectWithValue(error?.response?.data || { message : "error while fetching reviews"})
+    }
+});
+
+
+export const likeReview = createAsyncThunk('/reviews/likeReview',async(id,{rejectWithValue})=>{
+    try {
+        const response = await axiosInstance.post(`/review/like/${id}`,{},{headers:{Authorization:localStorage.getItem('token')}});
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return rejectWithValue(error?.response?.data || { message : "error while fetching reviews"})
     }
 });
 
@@ -41,6 +66,46 @@ const reviewRatingSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         })   
+
+        // fetch reviews
+        .addCase(fetchReviews.pending,(state,action)=>{
+            state.loading = true;
+            state.error = null;
+        })
+
+        .addCase(fetchReviews.fulfilled,(state,action)=>{
+            state.reviews = action.payload;
+            state.loading = false;
+            state.error = null;
+        })
+
+        .addCase(fetchReviews.rejected,(state,action)=>{
+            state.loading = false;
+            state.error = action.payload;
+        }) 
+
+
+        // like reviews
+        .addCase(likeReview.pending,(state,action)=>{
+            state.loading = true;
+            state.error = null;
+        })
+
+        .addCase(likeReview.fulfilled,(state,action)=>{
+            const index = state.reviews.findIndex((review)=>{
+                return review._id == action.payload._id;
+            });
+
+            state.reviews.splice(index,1,action.payload);
+            // state.reviews = action.payload;
+            state.loading = false;
+            state.error = null;
+        })
+
+        .addCase(likeReview.rejected,(state,action)=>{
+            state.loading = false;
+            state.error = action.payload;
+        }) 
 
     }
 });
