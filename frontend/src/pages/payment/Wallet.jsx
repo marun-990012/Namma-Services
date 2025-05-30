d89b790f4127662d83b598b5f3d5f59a3e0c1d26
marun-990012 <marun990012@gmail.com>
2025-05-30 22:27:10 +0530

    in wallet page message text added

--- frontend/src/pages/payment/Wallet.jsx
+++ frontend/src/pages/payment/Wallet.jsx
@@ -24,30 +24,43 @@ function Wallet() {
   };
   return (
     
-    <div className="w-[31%] h-62  mx-auto p-6 bg-white rounded-2xl shadow-md border border-gray-200 space-y-4">
-      <div className="flex items-center justify-between">
-        <h2 className="text-xl font-semibold text-gray-800">💼 Wallet</h2>
-        <span className="text-sm bg-green-100 text-green-700 px-3 py-1 rounded-full font-medium">
-          Active
-        </span>
-      </div>
+    <div className="w-[31%] h-70 mx-auto p-6 bg-white rounded-2xl shadow-md border border-gray-200 space-y-4">
+  <div className="flex items-center justify-between">
+    <h2 className="text-xl font-semibold text-gray-800">💼 Wallet</h2>
+    <span className="text-sm bg-green-100 text-green-700 px-3 py-1 rounded-full font-medium">
+      Active
+    </span>
+  </div>
 
-      <div className="bg-gray-50 rounded-xl p-4 flex flex-col items-center text-center">
-        <p className="text-gray-600 text-sm mb-1">Your Total Coins</p>
-        <h3 className="text-4xl font-bold text-yellow-500">🪙 {userWallet?.coins}</h3>
-      </div>
+  <div className="bg-gray-50 rounded-xl p-4 flex flex-col items-center text-center">
+    <p className="text-gray-600 text-sm mb-1">Your Total Coins</p>
+    <h3 className="text-4xl font-bold text-yellow-500">🪙 {userWallet?.coins}</h3>
 
-      <button
-        onClick={handleAddCoins}
-        className="w-full flex items-center justify-center gap-2 bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 rounded-xl transition duration-200 ease-in-out shadow-sm hover:shadow-md"
-      >
-        Add Coins to Wallet
-      </button>
+  </div>
 
-       {showPopup && (
-        <AddCoins/>
-       )}
-    </div>
+  <div className="text-center">
+    {userWallet?.coins >= 1 ? (
+      <p className="text-green-600 text-sm mt-2 font-medium">
+        You have enough coins to send a job request.
+      </p>
+    ) : (
+      <p className="text-red-500 text-sm mt-2 font-medium">
+       Not enough coins to send a job request.
+      </p>
+    )}
+  </div>
+
+  <button
+    onClick={handleAddCoins}
+    className="w-full flex items-center justify-center gap-2 bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 rounded-xl transition duration-200 ease-in-out shadow-sm hover:shadow-md"
+  >
+    Add Coins to Wallet
+  </button>
+
+  
+  {showPopup && <AddCoins />}
+</div>
+
   );
 }
 export default Wallet;
