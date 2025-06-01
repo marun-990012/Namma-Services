b6ff8ff7a7c6dbf36ae31f6192c53cc801e2b828
marun-990012 <marun990012@gmail.com>
2025-06-01 23:05:43 +0530

    updated

--- frontend/src/pages/layout/MainLayout.jsx
+++ frontend/src/pages/layout/MainLayout.jsx
@@ -1,72 +1,63 @@
-import { Outlet,Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Outlet, Link , useNavigate} from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import Sidebar from "./SideBar";
-function MainLayout(){
-  const loginDetail = useSelector((state)=>{
+import { logout } from "../../redux/slices/authSlice";
+import { fetchAccount } from "../../redux/slices/profileSlice";
+
+function MainLayout() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const loginDetail = useSelector((state) => {
     return state.auth;
   });
 
-  console.log(loginDetail);
- const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true' || loginDetail.isLoggedIn;
+  // console.log(loginDetail);
+  const isLoggedIn = localStorage.getItem("token")
 
   console.log(isLoggedIn);
-    return(
-        <div className="min-h-screen bg-[#e3e7ef]">
-  {/* Top Navigation Bar */}
-  {isLoggedIn && (
-    <div className="bg-white text-black fixed top-0 left-0 w-full h-10 flex items-center px-4 shadow z-50">
-    <Link to="/login" className="mr-4 hover:underline">
-      Login
-    </Link>
-    <Link to="/register" className="hover:underline">
-      Register
-    </Link>
-  </div>
-  )}
 
-  {/* Page Content Below the Navbar */}
-  <div className="flex pt-11 ">
-    {/* Sidebar */}
-    {isLoggedIn &&(
-      <div className=" ml-1 rounded-[10px] shadow fixed left-0  z-40 ">
-      
-        {/* <Link to="/profile" className="hover:underline">
-        profile
-      </Link>
+  const handleLogout = () => {
+    dispatch(logout());
+    // dispatch(fetchAccount());
+    navigate("/login");
+  };
 
-      <Link to="/category/new" className="hover:underline">
-        create service
-      </Link>
+  return (
+    <div className="min-h-screen bg-[#e3e7ef]">
+      {/* Top Navigation Bar */}
+      {isLoggedIn && (
+        <div className="bg-white text-black fixed top-0 left-0 w-full h-10 flex items-center justify-between px-4 shadow z-50">
+          <div>
+            <Link to="/login" className="mr-4 hover:underline">
+              Login
+            </Link>
+            <Link to="/register" className="hover:underline">
+              Register
+            </Link>
+          </div>
 
-       <Link to="/service-category" className="hover:underline">
-        services
-      </Link>
+          <div>
+            <button onClick={handleLogout}>logout</button>
+          </div>
+        </div>
+      )}
 
-      <Link to="/job/post/new" className="hover:underline">
-        job post
-      </Link>
-      
-      <Link to="/jobs/recent" className="hover:underline">
-        job Recent
-      </Link>
+      {/* Page Content Below the Navbar */}
+      <div className="flex pt-11 ">
+        {/* Sidebar */}
+        {isLoggedIn && (
+          <div className=" ml-1 rounded-[10px] shadow fixed left-0  z-40 ">
+            
+            <Sidebar />
+          </div>
+        )}
 
-      <Link to="/dashboard" className="hover:underline">
-        Dashboard
-      </Link> */}
-      
-      <Sidebar/>
-      
-      
+        {/* Main Content Area */}
+        <div className=" px-2 w-full ml-23 ">
+          <Outlet />
+        </div>
+      </div>
     </div>
-    )}
-
-    {/* Main Content Area */}
-    <div className=" px-2 w-full ml-23 ">
-      <Outlet />
-    </div>
-  </div>
-</div>
-
-    )
+  );
 }
 export default MainLayout;
