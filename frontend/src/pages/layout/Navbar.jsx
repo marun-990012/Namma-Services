2bb0cc4e5c3f03a318d044dac56d9c19b75ec124
marun-990012 <marun990012@gmail.com>
2025-06-02 23:34:43 +0530

    navbar page created

---
+++ frontend/src/pages/layout/Navbar.jsx
@@ -0,0 +1,57 @@
+import {Link, useNavigate } from "react-router-dom";
+import { IconBellFilled } from "@tabler/icons-react";
+import {CirclePower,Kanban } from 'lucide-react';
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { logout } from "../../redux/slices/authSlice";
+
+function Navbar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const notificationCount = useSelector((state) => state.notifications?.unreadCount);
+  console.log(notificationCount);
+
+//   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+const isLoggedIn = localStorage.getItem('token');
+
+useEffect(() => {
+  if (!isLoggedIn) {
+    navigate("/login");
+  }
+}, [isLoggedIn, navigate]);
+
+  const handleLogout = async () => {
+    await dispatch(logout());
+    navigate("/login");
+  };
+
+  return (
+    <div className="flex items-center justify-between w-full">
+    
+      <div className="flex items-center gap-6">
+        <p className="flex items-center">Namma Services</p>
+      </div>
+
+      {/* Right Section */}
+      <div className="flex items-center gap-4">
+        <Link to="/notifications" className="relative text-yellow-700 hover:text-blue-600 transition">
+          <IconBellFilled size={24} />
+          {/* {notificationCount > 0 && ( */}
+            <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+              {notificationCount}
+            </span>
+          {/* )} */}
+        </Link>
+        <button
+          onClick={handleLogout}
+          className="flex items-center border border-gray-300 text-gray-600 px-3 py-1 rounded-md text-sm font-medium hover:bg-red-100 transition"
+        >
+        <CirclePower size={18} className="mr-1"/>  Logout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
