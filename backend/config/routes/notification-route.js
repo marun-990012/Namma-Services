c76c5c698d216957a50bd835b06365e2f66786ff
marun-990012 <marun990012@gmail.com>
2025-06-02 23:34:14 +0530

    notification routes added in server

---
+++ backend/config/routes/notification-route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import authentication from '../../app/middlewares/user-authentication.js';
+// import notificationController from '../../app/controllers/notification-controller.js'
+import notificationController from '../../app/controllers/notification-controller.js';
+
+const notificationRoute = express.Router();
+
+notificationRoute.get('/list/unread/count',authentication,notificationController.listUnreadCount);
+notificationRoute.get('/list/all',authentication,notificationController.getAll);
+notificationRoute.put('/read',authentication,notificationController.markAsRead);
+
+export default notificationRoute;
