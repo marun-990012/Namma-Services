d638607fc65ded35a05225c0230c5fab8401efda
marun-990012 <marun990012@gmail.com>
2025-06-02 23:40:21 +0530

    updated

--- frontend/src/pages/payment/Payment.jsx
+++ frontend/src/pages/payment/Payment.jsx
@@ -5,7 +5,7 @@ import Transactions from "./Transactions";
 function Payment() {
     
   return (
-    <div className="flex justify-center items-center border-3 border-white shodow-xl rounded-[8px] p-5 mb-4">
+    <div className=" bg-gray-100 flex justify-center items-center border-3 border-white shodow-xl rounded-[8px] p-5 mb-4">
       <div className="flex w-full justify-between">
          <Wallet/>
         <Transactions/>
