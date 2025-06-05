a2674a1a2b7b37ee28f4f00b5f8ddc523b41ef2e
marun-990012 <marun990012@gmail.com>
2025-06-05 23:08:20 +0530

    updated

--- backend/app/controllers/transaction-controller.js
+++ backend/app/controllers/transaction-controller.js
@@ -6,12 +6,46 @@ transactionController.history = async (req,res)=>{
     console.log('transactions')
     try {
         const transaction = await Transaction.find({user:req.userId});
-
+        // console.log(transaction)
         return res.status(200).json(transaction);
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: 'Internal server error' });
     }
 }
+
+
+import mongoose from 'mongoose';
+const ObjectId = mongoose.Types.ObjectId;
+
+transactionController.totalRevenue = async (req, res) => {
+  try {
+    const result = await Transaction.aggregate([
+      {
+        $match: {
+          user: new ObjectId(req.userId), // Ensure it's an ObjectId
+          purpose: 'salary_payment',
+          status: 'success',
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          total: { $sum: "$amount" },
+          count: { $sum: 1 }
+        }
+      }
+    ]);
+
+    const totalRevenue = result[0]?.total || 0;
+    console.log("Aggregation Result:", result);
+    return res.status(200).json({ totalRevenue });
+  } catch (error) {
+    console.error("Aggregation error:", error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
 
 export default transactionController;
