209b6192bb3417a1ff4ef74e9e414fc85c55bfc0
marun-990012 <marun990012@gmail.com>
2025-06-05 23:06:45 +0530

    updated

--- frontend/src/hooks/useConsiderServiceProvider.jsx
+++ frontend/src/hooks/useConsiderServiceProvider.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {considerServiceProvider } from "../redux/slices/jobPostSlice";
 import toast from "react-hot-toast";
+import { showJobPostDetail } from "../redux/slices/jobPostSlice";
 
 export const useConsiderServiceProvider = () => {
   const dispatch = useDispatch();
@@ -11,6 +12,7 @@ export const useConsiderServiceProvider = () => {
    try {
       const res = await dispatch(considerServiceProvider(formData)).unwrap();
       toast.success("Service provider has been consider for job.");
+      dispatch(showJobPostDetail(formData.id));
       
     } catch (error) {
       toast.error(error.message || "Failed to consider the service provider. Please try again.");
