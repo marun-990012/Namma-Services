f692114fdd401479ac6c8de651878e28ddeca3a4
marun-990012 <marun990012@gmail.com>
2025-06-05 23:08:02 +0530

    updated

--- backend/config/routes/transaction-route.js
+++ backend/config/routes/transaction-route.js
@@ -6,5 +6,6 @@ import authentication from '../../app/middlewares/user-authentication.js';
 const transactionRoute = express.Router();
 
 transactionRoute.get('/history',authentication,transactionController.history);
+transactionRoute.get('/revenue',authentication,transactionController.totalRevenue);
 
 export default transactionRoute;
